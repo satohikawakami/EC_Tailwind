500d1e9c9ca59ec6d70ace5465caa632cc15fe6a
github satoshikawakami <s.kawakami0816@gmail.com>
2022-07-21 09:29:19 +0900

    プロジェクト始動

---
+++ src/main/java/com/exampe/demo/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.exampe.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class HomeController {
+	
+	@GetMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("title", "ようこそ");
+		return "home";
+	}
+
+}
